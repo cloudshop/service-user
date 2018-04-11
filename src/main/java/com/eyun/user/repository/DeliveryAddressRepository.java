17d965c48dc85216cd227ef83026cc33d01bf1ad
1203344821@qq.com <123456xy>
2018-04-11 20:47:10 +0800

    删除无用

--- src/main/java/com/eyun/user/repository/DeliveryAddressRepository.java
+++ src/main/java/com/eyun/user/repository/DeliveryAddressRepository.java
@@ -1,9 +1,13 @@
 package com.eyun.user.repository;
 
 import com.eyun.user.domain.DeliveryAddress;
+import com.eyun.user.service.dto.DeliveryAddressDTO;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
 
 
 /**
@@ -12,5 +16,10 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long>, JpaSpecificationExecutor<DeliveryAddress> {
+
+
+
+    @Query(value = " SELECT * FROM delivery_address AS d LEFT JOIN user_annex u ON d.user_annex_id=u.id WHERE u.id:id",nativeQuery = true)
+    List<DeliveryAddressDTO> findDeliveryAddressList(@Param("id") Long id);
 
 }
