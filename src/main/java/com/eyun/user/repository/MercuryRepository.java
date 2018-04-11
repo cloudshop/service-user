8324912d3e41640f57e1c5dca8b8864fbcd5d547
1203344821@qq.com <123456xy>
2018-04-11 15:55:13 +0800

    附近商户

--- src/main/java/com/eyun/user/repository/MercuryRepository.java
+++ src/main/java/com/eyun/user/repository/MercuryRepository.java
@@ -1,9 +1,13 @@
 package com.eyun.user.repository;
 
 import com.eyun.user.domain.Mercury;
+import com.eyun.user.service.dto.MercuryDTO;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
+
+import java.util.List;
 
 
 /**
@@ -11,6 +15,9 @@ import org.springframework.data.jpa.repository.*;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface MercuryRepository extends JpaRepository<Mercury, Long>, JpaSpecificationExecutor<Mercury> {
+public interface MercuryRepository extends JpaRepository<Mercury, Long>, JpaSpecificationExecutor<Mercury>{
 
+
+    @Query(nativeQuery = true, value = "SELECT * from mercury ")
+    List<MercuryDTO> findNearMerchantsList();
 }
