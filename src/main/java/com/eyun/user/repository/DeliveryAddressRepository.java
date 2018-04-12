af11a409e29ca47af1e6f1d689369288ebd9ba61
1203344821@qq.com <123456xy>
2018-04-12 16:49:50 +0800

    添加注释

--- src/main/java/com/eyun/user/repository/DeliveryAddressRepository.java
+++ src/main/java/com/eyun/user/repository/DeliveryAddressRepository.java
@@ -8,10 +8,12 @@ import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.*;
 
 import java.util.List;
+import java.util.Map;
 
 
 /**
  * Spring Data JPA repository for the DeliveryAddress entity.
+ * @author Jack_wen
  */
 @SuppressWarnings("unused")
 @Repository
@@ -19,7 +21,7 @@ public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress
 
 
 
-    @Query(value = " SELECT d.city,d.phone FROM delivery_address AS d LEFT JOIN user_annex u ON d.user_annex_id=u.id WHERE u.id:id",nativeQuery = true)
-    List<DeliveryAddressDTO> findDeliveryAddressList(@Param("id") Long id);
+    @Query(value = " SELECT d.city,d.phone FROM delivery_address AS d LEFT JOIN user_annex u ON d.user_annex_id=u.id WHERE u.id=:id",nativeQuery = true)
+    Map findDeliveryAddressList(@Param("id")Long id);
 
 }
