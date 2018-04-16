213b630eb98ca870d42c28c68df5cb163190d734
1203344821@qq.com <123456xy>
2018-04-17 01:20:44 +0800

    查询用户详情接口参数修改

--- src/main/java/com/eyun/user/web/rest/UserAnnexResource.java
+++ src/main/java/com/eyun/user/web/rest/UserAnnexResource.java
@@ -24,6 +24,7 @@ import org.springframework.web.bind.annotation.*;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
