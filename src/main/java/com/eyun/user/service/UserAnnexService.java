64ad9a032024331dd709f5c5e2e123c238033a4c
1203344821@qq.com <123456xy>
2018-04-17 01:20:03 +0800

    查询用户详情接口参数修改

--- src/main/java/com/eyun/user/service/UserAnnexService.java
+++ src/main/java/com/eyun/user/service/UserAnnexService.java
@@ -3,6 +3,8 @@ package com.eyun.user.service;
 import com.eyun.user.service.dto.UserAnnexDTO;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
