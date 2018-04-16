db37fe2aa8629cf50e504bf978973dae4fd1b1d7
1203344821@qq.com <123456xy>
2018-04-17 00:19:50 +0800

    查询用户详情接口编写

--- src/main/java/com/eyun/user/repository/UserAnnexRepository.java
+++ src/main/java/com/eyun/user/repository/UserAnnexRepository.java
@@ -1,9 +1,13 @@
 package com.eyun.user.repository;
 
 import com.eyun.user.domain.UserAnnex;
+import com.eyun.user.service.dto.UserAnnexDTO;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
+
+import java.util.Map;
 
 
 /**
@@ -12,5 +16,8 @@ import org.springframework.data.jpa.repository.*;
 @SuppressWarnings("unused")
 @Repository
 public interface UserAnnexRepository extends JpaRepository<UserAnnex, Long>, JpaSpecificationExecutor<UserAnnex> {
+
+    @Query(value = " select u.phone,u.avatar,u.nickname from user_annex AS u where userid=:id",nativeQuery = true)
+    Map userInfo(@Param("id")Long id);
 
 }
