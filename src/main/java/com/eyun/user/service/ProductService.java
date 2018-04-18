6501b8cb234fc8f5dae63fa2f83d092aa983e98a
1203344821@qq.com <123456xy>
2018-04-18 17:13:01 +0800

    商品列表方法的编写

---
+++ src/main/java/com/eyun/user/service/ProductService.java
@@ -0,0 +1,21 @@
+package com.eyun.user.service;
+
+import com.eyun.user.client.AuthorizedFeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ *
+ * @author Jack_wen
+ * @date 2018/4/18
+ */
+@AuthorizedFeignClient(name="product")
+public interface ProductService {
+
+    @GetMapping("/api/product/shop")
+    List<Map> ProductList(@RequestParam("shopId") Long id);
+
+}
