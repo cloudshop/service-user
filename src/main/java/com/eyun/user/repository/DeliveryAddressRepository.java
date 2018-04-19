01afde5a1171c421455d5e48c7b0ce40a3eaff4a
1203344821@qq.com <123456xy>
2018-04-19 16:45:06 +0800

    地址接口参数调整

--- src/main/java/com/eyun/user/repository/DeliveryAddressRepository.java
+++ src/main/java/com/eyun/user/repository/DeliveryAddressRepository.java
@@ -3,6 +3,7 @@ package com.eyun.user.repository;
 import com.eyun.user.domain.DeliveryAddress;
 import com.eyun.user.service.dto.DeliveryAddressDTO;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
@@ -20,7 +21,7 @@ import java.util.Map;
 public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long>, JpaSpecificationExecutor<DeliveryAddress> {
 
 
-    @Query(value = " SELECT d.city,d.phone,d.contact,d.id  FROM delivery_address AS d LEFT JOIN user_annex u ON d.user_annex_id=u.id WHERE u.id=:id",nativeQuery = true)
+    @Query(value = " SELECT ifnull(d.city,\"\") AS city, ifnull(d.phone,\"\") AS phone,ifnull(d.contact,\"\") AS contact,ifnull(d.id,\"\") AS id,ifnull(d.default_address,0) AS default_address   FROM delivery_address AS d LEFT JOIN user_annex u ON d.user_annex_id=u.id WHERE u.id=:id",nativeQuery = true)
     List<Map> findDeliveryAddressList(@Param("id")Long id);
 
 
