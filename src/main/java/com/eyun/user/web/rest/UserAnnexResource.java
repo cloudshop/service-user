cbd2b31db28eff0864dc7b71ea29fe122ba283d9
1203344821@qq.com <123456xy>
2018-04-19 16:50:26 +0800

    用户接口参数调整

--- src/main/java/com/eyun/user/web/rest/UserAnnexResource.java
+++ src/main/java/com/eyun/user/web/rest/UserAnnexResource.java
@@ -144,11 +144,11 @@ public class UserAnnexResource {
         return ResponseEntity.ok().body(null);
     }
 
-    @GetMapping("/user-annexes/userInfo/{id}/")
+    @GetMapping("/user-annexes/userInfo/")
     @Timed
-    public ResponseEntity userInfo(@PathVariable Long id){
-        //UserDTO account = uaaService.getAccount();
-        UserAnnex userAnnex = userAnnexService.userInfo(id);
+    public ResponseEntity userInfo(){
+        UserDTO account = uaaService.getAccount();
+        UserAnnex userAnnex = userAnnexService.userInfo(account.getId());
         return ResponseUtil.wrapOrNotFound(Optional.ofNullable(userAnnex));
 
     }
