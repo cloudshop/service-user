e4ff6e559df71d7fff159ba1e263b0ddfbd2a8ae
wgw <756898059@qq.com>
2018-05-02 11:18:29 +0800

    直接邀请增值商家奖励100元 api

--- src/main/java/com/eyun/user/service/walletService.java
+++ src/main/java/com/eyun/user/service/walletService.java
@@ -1,8 +1,11 @@
 package com.eyun.user.service;
 
 import com.eyun.user.client.AuthorizedFeignClient;
+import com.eyun.user.service.dto.IncrementUserRewardDTO;
 import com.eyun.user.service.dto.ServiceProviderRewardDTO;
+
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 /**
  * Created by Jack_wen on 2018/4/27.
@@ -12,4 +15,8 @@ public interface walletService {
 
     @PutMapping("/api/serviceProvider/reward")
     void invitationDeductions( ServiceProviderRewardDTO serviceProviderRewardDTO);
+    
+    @PutMapping("/api/incrementUser/reward")
+    void incrementUserReward(@RequestBody IncrementUserRewardDTO incrementUserRewardDTO);
+    
 }
